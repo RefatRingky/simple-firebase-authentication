0146cc661cf02e834779e06e090fbb4f843a1676
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-04-07 03:09:19 +0600

    google sign in set

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import './App.css';
  import app from './firebase.init';
  import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from 'react';
  const auth = getAuth(app);
 
 
 function App() {
+  const [user,setUser] = useState();
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn =()=>{
-    signInWithPopup(auth,provider)
+    signInWithPopup(auth, provider)
     .then(result =>{
       const user =result.user;
+      setUser(user);
       console.log(user);
     })
     .catch(error=>{
-      console.log(error);
+      console.error('error',error)
     })
   }
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Googl Sign In</button>
+      <h2>Name:{user.displayName}</h2>
+      <p>I know your Email Address:{user.email}</p>
+      <img src={user.photoURL} alt=''/>
+      
     </div>
   );
 }
